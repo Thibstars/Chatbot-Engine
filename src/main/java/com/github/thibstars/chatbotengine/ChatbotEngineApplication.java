5e6f621283fd8013129291200d5f969a53f5cf0a
Thibstars <thibault.helsmoortel@gmail.com>
2019-12-26 13:31:02 +0100

    Initial commit

---
+++ src/main/java/com/github/thibstars/chatbotengine/ChatbotEngineApplication.java
@@ -0,0 +1,13 @@
+package com.github.thibstars.chatbotengine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChatbotEngineApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ChatbotEngineApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/github/thibstars/chatbotengine/ChatbotEngineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.thibstars.chatbotengine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChatbotEngineApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
